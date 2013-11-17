b4ea7d806ee89267afb00f85ce7b9bc7449d6a7b
Carwyn Pelley <cpelley.pub@gmail.com>
2013-11-17 14:59:55 +0000

    ENH: Working square diamond algorithm.

--- fractal.py
+++ fractal.py
@@ -1,26 +1,7 @@
 import numpy as np
 import numpy.lib.stride_tricks as stride
-from mpl_toolkits.mplot3d import axes3d
+import mpl_toolkits.mplot3d
 import matplotlib.pyplot as plt
-
-
-#size = (257, 257)
-#size = (65, 65)
-#size = (33, 33)
-size = (17, 17)
-#size = (5, 5)
-#size = (3, 3)
-
-dat = np.ma.zeros(size, dtype=np.float)
-#dat.mask = True
-
-np.random.seed(0)
-corners = np.array([[0, 0], [0, -1], [-1, -1], [-1, 0]])
-corner_x = np.array([0, 0, -1, -1])
-corner_y = np.array([0, -1, -1, 0])
-dat[corner_x, corner_y] = np.random.rand(4) * 1
-
-iterations = (np.log2(size[0] - 1).astype(int), np.log2(size[1] - 1).astype(int))
 
 
 def window_views(xsize=3, ysize=3, xstep=1, ystep=1):
@@ -39,24 +20,26 @@ def window_views(xsize=3, ysize=3, xstep=1, ystep=1):
         Window dim-1 step, corresponding to window center.
 
     Returns:
+        Numpy ndarray which represents a view of the grid.
 
-    Numpy ndarray which represents a view of the grid.
+    .. note::
 
+    This function is not currently in use.
 
     """
-    strides = (dat.strides[0] * xstep,
-               dat.strides[1] * ystep,
-               dat.strides[0],
-               dat.strides[1])
-    window = (((dat.shape[0] - 1) / (xsize - 1)),
-              ((dat.shape[1] - 1) / (ysize - 1)),
+    strides = (grid.strides[0] * xstep,
+               grid.strides[1] * ystep,
+               grid.strides[0],
+               grid.strides[1])
+    window = (((grid.shape[0] - 1) / (xsize - 1)),
+              ((grid.shape[1] - 1) / (ysize - 1)),
               xsize,
               ysize)
-    all_windows = stride.as_strided(dat, window, strides)
+    all_windows = stride.as_strided(grid, window, strides)
     return all_windows
 
 
-def subdivide_grid(iteration):
+def subdivide_grid(iteration, size):
     """
     Determine step size from subdivision number.
 
@@ -68,18 +51,16 @@ def subdivide_grid(iteration):
         dim_size_x, dim_size_y = (2**n + 1), (2**n + 1)
 
     Returns:
-
-    Dictionary with size of window and the step sizes toward their center
-    points information.
+        Dictionary with size of window and the step sizes toward their center
+        points information.
 
     """
-        
     xsize = (size[0] - 1) / (2 ** iteration)
     ysize = (size[1] - 1) / (2 ** iteration)
-    xstep = (xsize) / 2 
+    xstep = (xsize) / 2
     ystep = (ysize) / 2
 
-    # Double checking to ensrure compliance with algorithm.
+    # Double checking to ensure compliance with algorithm.
     if xstep != ystep:
         raise RuntimeError('step sizes do not match')
     if xsize != ysize:
@@ -89,11 +70,50 @@ def subdivide_grid(iteration):
 
 
 def perturbation(constant):
+    """
+    Provides a random number in the range [-constant, +constant].
+
+    Args:
+
+    * constant (float)
+
+    Returns:
+    Random number in the range [-constant, +constant].
+
+    """
     return np.random.rand() * 2 * constant - constant
 
 
-def square_det(xstep, ystep, step_x, step_y, wrap=True):
-    # Return index of all edge midpoints of surrounding point.
+def square_det(xstep, ystep, step_x, step_y, size, wrap=True):
+    """
+    Return index of all edge midpoints of surrounding point.
+
+    With 'O' representing the current location in the grid, 'o' represents
+    point in the immediate neighbourhood that are not used, while those marked
+    with 'X' have their location returned:
+
+    o X o
+    X O X
+    o X o
+
+    Args:
+
+    * xstep (int):
+        The number of indexes between points considered in 'x' (corresponds
+        to an iteratively subdivided grid).
+    * ystep (int):
+        The number of indexes between points considered in 'y' (corresponds
+        to an iteratively subdivided grid).
+    * step_x (int):
+        x Location of our point with respect to the entire grid.
+    * step_y (int):
+        y Location of our point with respect to the entire grid.
+
+    Returns:
+        x index ndarray and y index ndarray representing the location of each
+        surrounding point in the grid.
+
+    """
     # Performs wraparound by default.
     center_x = np.array([step_x - xstep,
                          step_x,
@@ -119,17 +139,58 @@ def square_det(xstep, ystep, step_x, step_y, wrap=True):
         center_y = center_y[mask]
 
     return center_y, center_x
-    
 
-def main(scale=0.5):
+
+def midpoint_sd(size, scale=0.5, random_corner=True):
+    """
+    Midpoint displacement algorithm.
+
+    The dimaond-square version is implemented.
+
+    Args:
+
+    * size (tuple):
+        (x, y) representing the resulting grid size.  This also indirectly
+        determines the number of subdivision iterations for the algorithm.
+        This is determined by the following relation:
+        size_0, size_1 = (2**s + 1), (2**s + 1) where 's' is the number of
+        iterations.  Rearranging for s = log_2(size_x - 1).  The dimension of
+        the grid must be equal and must abide by, 2**n + 1 where n in Z.
+    * scale (float):
+        Determines the scale of the resulting terrain.  This value has no
+        impact on the behaviour of the algorithm.
+    * random_corder (bool):
+        Specifies whether grid corner points are initialised at 0 (False) or
+        populated with random values (True).  By default grid corner points are
+        assigned random values (rand() * scale).
+
+    Returns:
+        ndarray grid representing a heightmap.
+
+    """
+    if np.log2(size[0] - 1) % 2:
+        raise ValueError('Grid size {} not allowed, 2**n + 1 accepted where '
+            'n in Z'.format(size[0]))
+    if size[0] != size[1]:
+        raise ValueError(
+            'Grid dimensions specified: {} are not equal'.format(size))
+
+    grid = np.ma.zeros(size, dtype=np.float)
+
+    if random_corner:
+        corner_x = np.array([0, 0, -1, -1])
+        corner_y = np.array([0, -1, -1, 0])
+        grid[corner_x, corner_y] = np.random.rand(4) * scale
+
+    iterations = (
+        np.log2(size[0] - 1).astype(int), np.log2(size[1] - 1).astype(int))
+
     for iteration in xrange(iterations[0]):
-        xsize, ysize, xstep, ystep = subdivide_grid(iteration)
+        xsize, ysize, xstep, ystep = subdivide_grid(iteration, size)
 
         # Diamond step.
-        for step_x in xrange(xstep, size[0], (xstep)*2):
-            for step_y in xrange(ystep, size[1], (ystep)*2):
-                # Each operation has -1 as array index from 0.  Cancels out
-                # with subtraction but through with addition.
+        for step_x in xrange(xstep, size[0], (xstep) * 2):
+            for step_y in xrange(ystep, size[1], (ystep) * 2):
                 center_x = np.array([step_x - xstep,
                                      step_x + xstep,
                                      step_x + xstep,
@@ -139,46 +200,67 @@ def main(scale=0.5):
                                      step_y - ystep,
                                      step_y - ystep])
 
-                dat[step_x, step_y] = (dat[center_y, center_x].mean() +
+                grid[step_x, step_y] = (grid[center_y, center_x].mean() +
                                        perturbation(scale))
 
         # Square step.
-        for step_x in xrange(xstep, size[0], (xstep)*2):
-            for step_y in xrange(ystep, size[1], (ystep)*2):
-                center_y, center_x = square_det(xstep, ystep, step_x, step_y)
+        for step_x in xrange(xstep, size[0], (xstep) * 2):
+            for step_y in xrange(ystep, size[1], (ystep) * 2):
+                center_y, center_x = square_det(
+                    xstep, ystep, step_x, step_y, size)
 
                 # Determine value for each of these midpoints.
                 for y, x in zip(center_y, center_x):
-                    # Add one to indexes as I counter indexes in square_det
                     center_in_y, center_in_x = square_det(
-                        xstep, ystep, x, y, wrap=False)
-                    dat[y, x] = (dat[center_in_y, center_in_x].mean() +
+                        xstep, ystep, x, y, size, wrap=False)
+                    grid[y, x] = (grid[center_in_y, center_in_x].mean() +
                                  perturbation(scale))
 
         # Update perturbation constant.
-        pert_cons /= 2.#*= np.power(2, -1.)
+        scale /= 2.  # *= np.power(2, -1.)
 
-        #print dat[xstep - 1:: xstep, ystep - 1:: ystep]
         print 'iteration: {} of {} completed'.format(
             iteration + 1, iterations[0])
         print 'xsize: {}, ysize: {}, xstep: {}, ystep: {}\n'.format(
             xsize, ysize, xstep, ystep)
 
+    return grid
 
-def plot_grid():
+
+def plot_grid(grid):
+    """
+    Simple plotting wrapper for plotting our resulting heightmap grid.
+
+    Resulting 3D wireframe automatically determines suitable z-limits based on
+    value ranges.
+
+    Args:
+
+    * grid (:class:`numpy.array`):
+        A grid representing a heightmap.
+
+    Returns:
+        None
+
+    """
     fig = plt.figure()
     ax = fig.add_subplot(121, projection='3d')
-    #ax.set_zlim3d(-10, 10)
-    xx, yy = np.meshgrid(np.arange(size[0]), np.arange(size[1]))
-    ax.plot_wireframe(xx, yy, dat, color='b')
-    ax.set_zlim3d(-2, 2)
+    xx, yy = np.meshgrid(np.arange(grid.shape[0]), np.arange(grid.shape[1]))
+    ax.plot_wireframe(xx, yy, grid, color='b')
+    diff = grid.max() - grid.min()
+    ax.set_zlim3d(grid.min() - (3 * diff), grid.max() + (3 * diff))
 
     ax2 = fig.add_subplot(122)
-    mesh = ax2.pcolormesh(dat)
+    mesh = ax2.pcolormesh(grid)
     plt.colorbar(mesh)
 
 
 if __name__ == '__main__':
-    main()
-    plot_grid()
+    size = ((257, 257), (65, 65), (33, 33), (17, 17), (5, 5), (3, 3))
+    size = size[1]
+
+    np.random.seed(0)
+
+    grid = midpoint_sd(size)
+    plot_grid(grid)
     plt.show()
